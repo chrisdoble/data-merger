dc90173b246b7d93ca3b60725b2459a7c48b8da8
Chris Doble <chris@chrisdoble.com>
2024-10-11 14:20:52 +1100

    Move rendering of pixmaps into DataAlignmentView

--- datamerger/widget/data_alignment_view.py
+++ datamerger/widget/data_alignment_view.py
@@ -1,7 +1,9 @@
+import numpy as np
 from pewlib import Laser
 from PySide6 import QtCore, QtGui, QtWidgets
 
 from datamerger.io.sized_data import SizedData
+from .turbo_color_table import turbo_color_table
 
 
 class DataAlignmentView(QtWidgets.QGraphicsView):
@@ -19,9 +21,45 @@ class DataAlignmentView(QtWidgets.QGraphicsView):
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
+        data = laser.get(laser.elements[0])
+        pixmap = make_pixmap_from_data(data)
+        pixmap_item = self.scene().addPixmap(pixmap)
+        self.centerOn(pixmap_item)
+
     def wheelEvent(self, event: QtGui.QWheelEvent):
         self.setTransformationAnchor(
             QtWidgets.QGraphicsView.ViewportAnchor.AnchorUnderMouse
         )
         scale = 2 ** (event.angleDelta().y() / 360.0)
         self.scale(scale, scale)
+
+
+def make_pixmap_from_data(data: np.ndarray) -> QtGui.QPixmap:
+    """Makes a pixmap from a two-dimensional array of data.
+
+    Uses the turbo colour table to colour the data.
+    """
+    assert data.ndim == 2, "Can't make an image from an array that isn't 2D"
+
+    # Determine the minimum and maximum values (ignoring NaNs).
+    minimum = np.nanmin(data)
+    maximum = np.nanpercentile(data, 99)
+
+    # Convert values to uint8s to be used as indices into the lookup table.
+    # If there are any NaNs numpy will warn that they're invalid and convert
+    # them to 0s. The context manager suppresses those warnings.
+    with np.errstate(invalid="ignore"):
+        data = (255 * (data - minimum) / (maximum - minimum)).astype(np.uint8)
+
+    # Create an image from the data.
+    image = QtGui.QImage(
+        data.data,
+        data.shape[1],
+        data.shape[0],
+        data.strides[0],
+        QtGui.QImage.Format.Format_Indexed8,
+    )
+    image.setColorTable(turbo_color_table)
+    image.setColorCount(len(turbo_color_table))
+
+    return QtGui.QPixmap.fromImage(image)
--- datamerger/wizard/align_profilometer_data_page.py
+++ datamerger/wizard/align_profilometer_data_page.py
@@ -1,8 +1,6 @@
-import numpy as np
-from pewlib.io.npz import load
-from PySide6 import QtGui, QtWidgets
+from PySide6 import QtWidgets
 
-from datamerger.widget import DataAlignmentView, turbo_color_table
+from datamerger.widget import DataAlignmentView
 from .load_data_page import LASER_FIELD_NAME, PROFILOMETER_DATA_FIELD_NAME
 
 
@@ -28,36 +26,9 @@ class AlignProfilometerDataPage(QtWidgets.QWizardPage):
     def initializePage(self) -> None:
         assert self.__data_alignment_view is None
         laser = self.field(LASER_FIELD_NAME)
-        sized_data = self.field(PROFILOMETER_DATA_FIELD_NAME)
-        self.__data_alignment_view = DataAlignmentView(laser, sized_data, self)
+        profilometer_data = self.field(PROFILOMETER_DATA_FIELD_NAME)
+        self.__data_alignment_view = DataAlignmentView(laser, profilometer_data, self)
         self.__layout.addWidget(self.__data_alignment_view)
-
-        data = laser.get(laser.elements[0])
-
-        # Determine the minimum and maximum values (ignoring NaNs).
-        minimum = np.nanmin(data)
-        maximum = np.nanpercentile(data, 99)
-
-        # Convert values to uint8s to be used as indices into the lookup table.
-        # If there are any NaNs numpy will warn that they're invalid and convert
-        # them to 0s. The context manager suppresses those warnings.
-        with np.errstate(invalid="ignore"):
-            data = (255 * (data - minimum) / (maximum - minimum)).astype(np.uint8)
-
-        # Create an image from the data.
-        image = QtGui.QImage(
-            data.data,
-            data.shape[1],
-            data.shape[0],
-            data.strides[0],
-            QtGui.QImage.Format.Format_Indexed8,
-        )
-        image.setColorTable(turbo_color_table)
-        image.setColorCount(len(turbo_color_table))
-
-        pixmap = QtGui.QPixmap.fromImage(image)
-        pixmap_item = self.__data_alignment_view.scene().addPixmap(pixmap)
-        self.__data_alignment_view.centerOn(pixmap_item)
 
     def cleanupPage(self) -> None:
         self.__layout.takeAt(0)
