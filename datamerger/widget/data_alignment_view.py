927f4ba73ae715f57593634217502a9c53db95e8
Chris Doble <chris@chrisdoble.com>
2024-10-17 15:29:37 +1100

    Disable controls while recalculating data

--- datamerger/widget/data_alignment_view.py
+++ datamerger/widget/data_alignment_view.py
@@ -31,14 +31,15 @@ class DataAlignmentView(QtWidgets.QWidget):
             self.__on_element_combo_box_current_index_changed
         )
 
-        rotate_button = QtWidgets.QPushButton("Rotate", self)
-        rotate_button.clicked.connect(self.__on_rotate_clicked)
-        rotate_button.setAutoDefault(False)
+        self.__rotate_button = QtWidgets.QPushButton("Rotate", self)
+        self.__rotate_button.clicked.connect(self.__on_rotate_clicked)
+        self.__rotate_button.setAutoDefault(False)
+        self.__rotate_button.setEnabled(False)
 
         # Lay out toolbar items horizontally.
         toolbar_layout = QtWidgets.QHBoxLayout()
         toolbar_layout.addWidget(self.__element_combo_box)
-        toolbar_layout.addWidget(rotate_button)
+        toolbar_layout.addWidget(self.__rotate_button)
         toolbar_layout.addStretch()
 
         # Set up the graphics view.
@@ -137,6 +138,65 @@ class DataAlignmentView(QtWidgets.QWidget):
         scale = 2 ** (event.angleDelta().y() / 360.0)
         self.__graphics_view.scale(scale, scale)
 
+    def __disable_controls(self) -> None:
+        self.__rotate_button.setEnabled(False)
+
+    def __enable_controls(self) -> None:
+        self.__rotate_button.setEnabled(True)
+
+    def __on_data_manipulator_success(
+        self, other_data_manipulated: np.ndarray, position: QtCore.QPoint
+    ) -> None:
+        if self.__other_data_pixmap_item is not None:
+            self.__scene.removeItem(self.__other_data_pixmap_item)
+
+        self.__other_data_manipulated = other_data_manipulated
+        self.__other_data_pixmap_item = self.__scene.addPixmap(
+            make_pixmap_from_data(other_data_manipulated)
+        )
+        self.__other_data_pixmap_item.setFlags(
+            QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemIsMovable
+        )
+        self.__other_data_pixmap_item.setOpacity(0.5)
+        self.__other_data_pixmap_item.setPos(position.toPointF())
+        self.__other_data_pixmap_item.setZValue(1)
+        self.__enable_controls()
+        self.__on_aligned_data_changed()
+
+    @QtCore.Slot()
+    def __on_element_combo_box_current_index_changed(self, index: int) -> None:
+        if self.__laser is None:
+            return
+
+        self.__element = self.__laser.elements[index]
+        self.__recreate_laser_pixmap_item()
+
+    @QtCore.Slot()
+    def __on_rotate_clicked(self) -> None:
+        # If a pixmap item doesn't already exist DataManipulator is probably
+        # running and we should wait until that's finished before rotating.
+        if self.__other_data_pixmap_item is None:
+            return
+
+        self.__rotation = (self.__rotation + 1) % 4
+
+        # We want to disable controls that can cause recalculation of the other
+        # data, and the wizard's next button until recalculation is complete. To
+        # disable the next button we clear __other_data_manipulated which causes
+        # aligned_data to return None, which in turn disables the next button.
+        self.__disable_controls()
+        self.__other_data_manipulated = None
+        self.__on_aligned_data_changed()
+
+        # Calculate the position of the new pixmap item such that it rotates
+        # around its centre (as opposed to keeping the top-left corner fixed).
+        position = self.__other_data_pixmap_item.pos().toPoint()
+        width, height = cast(
+            Tuple[int, int], self.__other_data_pixmap_item.pixmap().size().toTuple()
+        )
+        new_position = position + QtCore.QPoint(width - height, height - width) * 0.5
+        self.__recreate_other_data_pixmap_item(new_position)
+
     def __recreate_laser_pixmap_item(self) -> None:
         assert self.__laser is not None
         elemental_data = self.__laser.get(self.__element)
@@ -163,50 +223,6 @@ class DataAlignmentView(QtWidgets.QWidget):
         )
         data_manipulator.signals.success.connect(on_data_manipulator_success)
         QtCore.QThreadPool.globalInstance().start(data_manipulator)
-
-    def __on_data_manipulator_success(
-        self, other_data_manipulated: np.ndarray, position: QtCore.QPoint
-    ) -> None:
-        if self.__other_data_pixmap_item is not None:
-            self.__scene.removeItem(self.__other_data_pixmap_item)
-
-        self.__other_data_manipulated = other_data_manipulated
-        self.__other_data_pixmap_item = self.__scene.addPixmap(
-            make_pixmap_from_data(other_data_manipulated)
-        )
-        self.__other_data_pixmap_item.setFlags(
-            QtWidgets.QGraphicsItem.GraphicsItemFlag.ItemIsMovable
-        )
-        self.__other_data_pixmap_item.setOpacity(0.5)
-        self.__other_data_pixmap_item.setPos(position.toPointF())
-        self.__other_data_pixmap_item.setZValue(1)
-        self.__on_aligned_data_changed()
-
-    @QtCore.Slot()
-    def __on_element_combo_box_current_index_changed(self, index: int) -> None:
-        if self.__laser is None:
-            return
-
-        self.__element = self.__laser.elements[index]
-        self.__recreate_laser_pixmap_item()
-
-    @QtCore.Slot()
-    def __on_rotate_clicked(self) -> None:
-        # If a pixmap item doesn't already exist DataManipulator is probably
-        # running and we should wait until that's finished before rotating.
-        if self.__other_data_pixmap_item is None:
-            return
-
-        self.__rotation = (self.__rotation + 1) % 4
-
-        # Calculate the position of the new pixmap item such that it rotates
-        # around its centre (as opposed to keeping the top-left corner fixed).
-        position = self.__other_data_pixmap_item.pos().toPoint()
-        width, height = cast(
-            Tuple[int, int], self.__other_data_pixmap_item.pixmap().size().toTuple()
-        )
-        new_position = position + QtCore.QPoint(width - height, height - width) * 0.5
-        self.__recreate_other_data_pixmap_item(new_position)
 
     @property
     def __scene(self) -> QtWidgets.QGraphicsScene:
